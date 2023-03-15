6b6e2c8ca570f47fd9f2c33de0e3081fd21b0017
charlesik <charlesikilo@gmail.com>
2023-03-15 16:05:58 +0100

    build update

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -12,7 +12,6 @@
 <script setup>
     import CurrentList from "@/components/CurrentList.vue";
     import Header from "@/components/Header.vue";
-    import { RouterView } from "vue-router";
 </script>
 
 <style lang="scss" scoped>
